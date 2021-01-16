7e2f32fbe914cbb8d5a8a0825f6b49322cf49dae
deepdarkdeploy <77545413+deepdarkdeploy@users.noreply.github.com>
2021-01-16 22:33:49 +0300

    Update buid.sh

--- DPI_demo/buid.sh
+++ DPI_demo/buid.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-set -x
+set -e 
+set -x 
+
 cd /build
 rm -rf ./nDPI
-if [ -f "master.tar" ]; then
-	tar xvf master.tar
-else 
-	git clone -b netfilter https://github.com/vel21ripn/nDPI.git
-	tar cvf master.tar nDPI
+
+if [ -e "./vel21ripn-ndpi.tar" ]; then
+        tar xvf vel21ripn-ndpi.tar
+else
+        git clone https://github.com/vel21ripn/nDPI
+	tar cvf vel21ripn-ndpi.tar nDPI
 fi
+
 cd nDPI
 git checkout netfilter
 ./autogen.sh
@@ -21,9 +25,9 @@ KERNEL_VERSION=`ls /lib/modules/`
 # pkg-config xtables --cflags 
 ls /build/
 mkdir /build/destdir
+chmod 777 /build/destdir
 #declare -x  DESTDIR=/build/destdir/
 ls
-cp -f /build/dummy-Makefile example/Makefile
 sed -i -e 's/-DOPENDPI_NETFILTER_MODULE/$(shell pkg-config --cflags xtables)/g' ndpi-netfilter/ipt/Makefile && \
 sed -i -e 's@MODULES_DIR := /lib/modules/$(shell uname -r)@MODULES_DIR := /lib/modules/$(shell ls /lib/modules/)@g' ndpi-netfilter/src/Makefile && \
 sed -i -e 's/depmod -a/depmod -a $(shell ls \/lib\/modules\/)/g' ndpi-netfilter/src/Makefile && \
@@ -33,18 +37,19 @@ make -j9 && \
 make install && \
 make modules_install && \
 echo $?
-mkdir -p /build/destdir/usr/lib64/xtables/ && \
-cp /usr/lib64/xtables/libxt_NDPI.so /build/destdir/usr/lib64/xtables/ && \
-mkdir -p /build/destdir/lib/modules/$KERNEL_VERSION/extra/ && \
-cp /build/nDPI/ndpi-netfilter/src/xt_ndpi.ko /build/destdir/lib/modules/$KERNEL_VERSION/extra/xt_ndpi.ko && \
-cp /build/nDPI/ndpi-netfilter/src/xt_ndpi.ko /build/destdir/lib/modules/$KERNEL_VERSION/extra/xt_ndpi.ko-non-stripped && \
-cd /build/destdir/usr/lib64/xtables/ && \
-#ln -s libxt_NDPI.so libxt_ndpi.so && \
-# The above is not required since the build creates this file and a symlink
+mkdir -p /build/destdir/usr/lib/x86_64-linux-gnu/xtables && \
+cp /usr/lib/x86_64-linux-gnu/xtables/libxt_NDPI.so /build/destdir/usr/lib/x86_64-linux-gnu/xtables/ && \
+mkdir -p /build/destdir/lib/modules/$KERNEL_VERSION/ && \
+cp /build/nDPI/ndpi-netfilter/src/xt_ndpi.ko /build/destdir/lib/modules/$KERNEL_VERSION/xt_ndpi.ko && \
+cp /build/nDPI/ndpi-netfilter/src/xt_ndpi.ko /build/destdir/lib/modules/$KERNEL_VERSION/xt_ndpi.ko-non-stripped && \
+cd /build/destdir/usr/lib/x86_64-linux-gnu/xtables/ && \
+ln -s libxt_NDPI.so libxt_ndpi.so && \
 echo $?
 
-strip --strip-debug /build/destdir/lib/modules/$KERNEL_VERSION/extra/xt_ndpi.ko
+strip --strip-debug /build/destdir/lib/modules/$KERNEL_VERSION/xt_ndpi.ko
 
+set +x
+set +e
 #modprobe xt_ndpi && lsmod|grep ndpi
 #patch -p0 < /build/ipt-makefile.patch && \
 #patch -p0 < /build/src-makefile.patch && \
